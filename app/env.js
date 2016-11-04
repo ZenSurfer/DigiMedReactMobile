b2be619356c04324dc60ff7747872fd65f5ca573
don <dbenas@ascendant.com.ph>
2016-11-04 15:00:53 +0800

    update env

--- app/env.js
+++ app/env.js
@@ -2,17 +2,22 @@
 import SQLite from 'react-native-sqlite-storage'
 
 function Env() {
-        this.emrUrl = 'http://192.168.1.40/imd5/public/api/v2';
-        this.doctor = {id: 1, name: 'Don'}
-        this.db = function () {
-            return SQLite.openDatabase({name : "testDB"})
-        }
-        this.setDoctor = function(id, name) {
-            this.doctor = {id: id, name: name}
-        }
-        this.getDoctor = function() {
-            return this.doctor
-        }
+    this.doctor = {
+        userID: 29,
+        id: 3,
+        name: 'Don',
+        type: '',
+        initial: '',
+    }
+    this.db = function () {
+        return SQLite.openDatabase({name : "testDB"})
+    }
+    this.setDoctor = function(id, name) {
+        this.doctor = {id: id, name: name}
+    }
+    this.getDoctor = function() {
+        return this.doctor
+    }
 }
 
 module.exports = Env;
