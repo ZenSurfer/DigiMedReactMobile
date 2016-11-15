2c48d05d34f63cde994a3495de0f47fc6b62a229
don <dbenas@ascendant.com.ph>
2016-11-15 15:34:51 +0800

    update index

--- index.android.js
+++ index.android.js
@@ -16,10 +16,8 @@ class AwesomeProject extends Component {
         FCM.requestPermissions(); // for iOS
         FCM.getFCMToken().then(token => {
             console.log(token)
-            // store fcm token in your server
         });
         this.notificationUnsubscribe = FCM.on('notification', (notif) => {
-            // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
             console.log(notif)
             this.setState({completed : true})
             setTimeout(() => {
